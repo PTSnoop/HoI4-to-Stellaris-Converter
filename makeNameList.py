fdfc6de33c531c0ca31cd72a4a660863bdf2ec76
Andrew <ptsnoop@gmail.com>
2018-02-03 12:14:39 +0000

    Better unicode handling, reading ship names from Vic2toHoI4 mods

--- makeNameList.py
+++ makeNameList.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 
 import os,sys
+import re
 import random
 import naive_parser
 import unicodedata
 
-def removeAccents(input_str):
-    nfkd_form = unicodedata.normalize('NFKD', input_str)
-    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+def removeAccents(inputNames):
+    outputNames = []
+    for name in inputNames:
+        try:
+            nfkd_form = unicodedata.normalize('NFKD', name)
+            name = u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+            name = name.encode('utf-8').decode('ascii')
+            outputNames.append(name)
+        except UnicodeEncodeError:
+            pass
+        except UnicodeDecodeError:
+            pass
+    return outputNames
 
 def ClunkyStringSplit(string):
     outputs = []
@@ -47,10 +58,12 @@ def TemplateFill(data, filename, writefilename):
     for key in data:
         filedata = filedata.replace(key,data[key])
 
-    output = open(writefilename,'w')
+    output = open(writefilename,'w',encoding="utf-8")
     output.write(filedata)
 
 def MakeNameList(tag,hoi4path,destFolder):
+
+    includeGenericUnitNames = True
 
     namespath = hoi4path + "common/names/01_names.txt";
     if os.path.exists(namespath):
@@ -60,46 +73,60 @@ def MakeNameList(tag,hoi4path,destFolder):
         print("Reading names from " +      hoi4path + "common/names/00_names.txt")
         names = naive_parser.ParseSaveFile(hoi4path + "common/names/00_names.txt")
 
-    specialUnitNamePath = hoi4path + "common/units/names/00_"+tag+"_names.txt"
-    if os.path.exists(specialUnitNamePath):
-        print("Reading unique unit names from "+specialUnitNamePath)
-        unitnames = naive_parser.ParseSaveFile(specialUnitNamePath)
+    modUnitNamesPath = hoi4path + "common/units/names/01_names.txt"
+    if os.path.exists(modUnitNamesPath):
+        print("Reading unit names from modded "+modUnitNamesPath)
+        unitnames = naive_parser.ParseSaveFile(modUnitNamesPath)
+        includeGenericUnitNames = False
     else:
-        print("Could not find "+specialUnitNamePath)
-        print("Reading unit names from " +     hoi4path + "common/units/names/00_names.txt")
-        unitnames = naive_parser.ParseSaveFile(hoi4path + "common/units/names/00_names.txt")
+        specialUnitNamePath = hoi4path + "common/units/names/00_"+tag+"_names.txt"
+        if os.path.exists(specialUnitNamePath):
+            print("Reading unique unit names from "+specialUnitNamePath)
+            unitnames = naive_parser.ParseSaveFile(specialUnitNamePath)
+        else:
+            print("Could not find "+specialUnitNamePath)
+            print("Reading unit names from " +     hoi4path + "common/units/names/00_names.txt")
+            unitnames = naive_parser.ParseSaveFile(hoi4path + "common/units/names/00_names.txt")
 
     malenames = stringlist_drill(names,tag,"male","names")
     femalenames = stringlist_drill(names,tag,"female","names")
     surnames = stringlist_drill(names,tag,"surnames")
 
-    subs = stringlist_drill(unitnames,tag,"submarine","unique") + stringlist_drill(unitnames,tag,"submarine","generic")
-    destroyers = stringlist_drill(unitnames,tag,"destroyer","unique") + stringlist_drill(unitnames,tag,"destroyer","generic")
-    light_cruisers = stringlist_drill(unitnames,tag,"light_cruiser","unique") + stringlist_drill(unitnames,tag,"light_cruiser","generic")
-    heavy_cruisers = stringlist_drill(unitnames,tag,"heavy_cruiser","unique") + stringlist_drill(unitnames,tag,"heavy_cruiser","generic")
-    battle_cruisers = stringlist_drill(unitnames,tag,"battle_cruiser","unique") + stringlist_drill(unitnames,tag,"battle_cruiser","generic")
-    battleships = stringlist_drill(unitnames,tag,"battleship","unique") + stringlist_drill(unitnames,tag,"battleship","generic")
-    carriers = stringlist_drill(unitnames,tag,"carrier","unique") + stringlist_drill(unitnames,tag,"carrier","generic")
+    subs = stringlist_drill(unitnames,tag,"submarine","unique")
+    destroyers = stringlist_drill(unitnames,tag,"destroyer","unique")
+    light_cruisers = stringlist_drill(unitnames,tag,"light_cruiser","unique")
+    heavy_cruisers = stringlist_drill(unitnames,tag,"heavy_cruiser","unique")
+    battle_cruisers = stringlist_drill(unitnames,tag,"battle_cruiser","unique")
+    battleships = stringlist_drill(unitnames,tag,"battleship","unique")
+    carriers = stringlist_drill(unitnames,tag,"carrier","unique")
 
+    if includeGenericUnitNames:
+        subs += stringlist_drill(unitnames,tag,"submarine","generic")
+        destroyers += stringlist_drill(unitnames,tag,"destroyer","generic")
+        light_cruisers += stringlist_drill(unitnames,tag,"light_cruiser","generic")
+        heavy_cruisers += stringlist_drill(unitnames,tag,"heavy_cruiser","generic")
+        battle_cruisers += stringlist_drill(unitnames,tag,"battle_cruiser","generic")
+        battleships += stringlist_drill(unitnames,tag,"battleship","generic")
+        carriers += stringlist_drill(unitnames,tag,"carrier","generic")
 
     planetnames = subs
     ships = destroyers + light_cruisers + heavy_cruisers + battle_cruisers + battleships + carriers
 
     planetnames = [sub for sub in subs if not any(char.isdigit() for char in sub)]
 
-    if len(planetnames) < 10:
+    # Leave this in until I work out what accents Stellaris can't take
+    ships = removeAccents(ships)
+    planetnames = removeAccents(planetnames)
+    malenames = removeAccents(malenames)
+    femalenames = removeAccents(femalenames)
+    surnames = removeAccents(surnames)
+
+    if len(planetnames) < 20:
         extraplanets = ships[:]
         random.shuffle(extraplanets)
         planetnames += extraplanets
 
     print(len(planetnames))
-
-    # Leave this in until I work out what accents Stellaris can't take
-    ships = [removeAccents(s) for s in ships]
-    planetnames = [removeAccents(s) for s in planetnames]
-    malenames = [removeAccents(s) for s in malenames]
-    femalenames = [removeAccents(s) for s in femalenames]
-    surnames = [removeAccents(s) for s in surnames]
 
     templateData = {}
     templateData["&TAG&"] = tag
